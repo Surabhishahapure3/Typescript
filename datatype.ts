27b6b4237c955e247aa7b2d8c8e2e7f1a5f9e099
Surabhishahapure3 <121013210+Surabhishahapure3@users.noreply.github.com>
2024-08-13 11:23:53 +0530

    Add files via upload

---
+++ datatype.ts
@@ -0,0 +1,29 @@
+/*
+// Primitvie types:
+// number
+let age: number = 30;
+let price: number = 19.99;
+
+// string 
+
+let name: string = "Surabhi";
+let greeting: string = `Hello, ${name}`;
+
+//boolean
+let isActive: boolean = true;
+let isLoggedIn: boolean = false;
+
+// bigint
+let largeNumber: bigint = 1234567890123456789012345678901234567890n;
+
+
+// symbol: Represents a unique and immutable value often used as object property keys.
+let uniqueId: symbol = Symbol("id");
+
+// undefined:missing value
+let data: undefined = undefined;
+
+// null: empty value
+let empty: null = null;
+
+*/
